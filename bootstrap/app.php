0c3782b755ae75cd639fbd7408504c7578768ce8
firfree938-hash <firfree938@gmail.com>
2026-08-13 17:40:53 -0500

    Update app.php
    Volver       $middleware->trustProxies(at: '*');

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
 
+      $middleware->trustProxies(at: '*');
 
         $middleware->alias([
             'edad' => CheckEdad::class,
