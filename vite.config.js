01a066e10885e6558de5a743eb93173cf8c6c4b3
firfree938-hash <firfree938@gmail.com>
2026-08-13 17:53:05 -0500

    Update vite.config.js
    prueba

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
                 'resources/js/app.js',
                 'resources/css/usuarios/index.css',
                 ...auth,
-                'resources/css/usuarios/view.css'
+
                 
             ],
             refresh: true,
